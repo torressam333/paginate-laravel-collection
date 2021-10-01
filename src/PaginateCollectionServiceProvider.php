b30b0e84770fbc554a56d6e0872ea2f7b8536e5f
sam <samuel.torres@goodwillaz.org>
2021-10-01 15:31:48 -0700

    Fix formatting in service provider

--- src/PaginateCollectionServiceProvider.php
+++ src/PaginateCollectionServiceProvider.php
@@ -14,11 +14,10 @@ class PaginateCollectionServiceProvider extends ServiceProvider
     public function register()
     {
         $file = app_path('Helpers/helpers.php');
+
         if (file_exists($file)) {
             require_once $file;
         }
-
-
     }
 
     /**
